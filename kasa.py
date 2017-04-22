9d2db5c2fcd0507e338d7b133250dc980ef81117
Topher Brown <topher200@gmail.com>
2017-04-22 16:51:36 -0400

    toggle state on SINGLE

--- kasa.py
+++ kasa.py
@@ -5,18 +5,42 @@ import requests
 import config
 
 
-OFF_STATE = "{\"system\":{\"set_relay_state\":{\"state\":0}}}"
-ON_STATE = "{\"system\":{\"set_relay_state\":{\"state\":1}}}"
+OFF_STATE = 0
+ON_STATE = 0
+OFF_STATE_REQUEST = "{\"system\":{\"set_relay_state\":{\"state\":0}}}"
+ON_STATE_REQUEST = "{\"system\":{\"set_relay_state\":{\"state\":1}}}"
 GET_SYS_INFO = "{\"system\":{\"get_sysinfo\":{}}}"
 
 
 def lambda_handler(event, _):
     print("Received event: " + json.dumps(event, indent=2))
 
+    # get current status
+    url = 'https://%s/?token=%s' % (config.KASA_URL, config.KASA_TOKEN)
+    sys_info_request_payload = {
+        'method': 'passthrough',
+        'params': {
+            'deviceId': config.KASA_DEVICE_ID,
+            'requestData': GET_SYS_INFO
+        }
+    }
+    status_response = requests.post(url, json=sys_info_request_payload).json()
+    print ('sys_info response: "%s"' % status_response)
+    current_state = json.loads(
+        status_response['result']['responseData']
+    )['system']['get_sysinfo']['relay_state']
+
     if event['clickType'] == 'SINGLE':
-        state = OFF_STATE
+        if current_state == OFF_STATE:
+            print('current state is OFF')
+            state = ON_STATE_REQUEST
+        else:
+            print('current state is ON')
+            state = OFF_STATE_REQUEST
     if event['clickType'] == 'DOUBLE':
-        state = ON_STATE
+        state = OFF_STATE_REQUEST
+    if event['clickType'] == 'LONG':
+        return
 
     url = 'https://%s/?token=%s' % (config.KASA_URL, config.KASA_TOKEN)
     payload = {
@@ -31,4 +55,4 @@ def lambda_handler(event, _):
     res = requests.post(url, json=payload)
     res.raise_for_status()
 
-    print('response: "%s"' % res.json())
+    print('change state response: "%s"' % res.json())
